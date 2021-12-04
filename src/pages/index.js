c00627de5572b67406b3684bd85295d96590098f
Amber Abreu <ambermabreu@gmail.com>
2021-12-04 16:48:29 -0500

    added technology images

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,29 @@
 import React from "react";
 import * as style from "../style/index.module.css";
 import { StaticImage } from "gatsby-plugin-image";
+
+import html from "../images/tech/html5.svg";
+import css from "../images/tech/css3.svg";
+import javascript from "../images/tech/javascript.svg";
+import react from "../images/tech/react.svg";
+import sequelize from "../images/tech/sequelize.svg";
+import postgres from "../images/tech/postgresql.svg";
+import express from "../images/tech/expressjs.svg";
+import node from "../images/tech/nodejs.svg";
+import redux from "../images/tech/redux.svg";
+
 export default function IndexPage() {
+  const technologies = [
+    "../images/tech/html5.svg",
+    "../images/tech/css3.svg",
+    "../images/tech/javascript.svg",
+    "../images/tech/react.svg",
+    "../images/tech/sequelize.svg",
+    "../images/tech/postgersql.svg",
+    "../images/tech/nodejs.svg",
+    "../images/tech/expressjs.svg",
+    "../images/tech/sequelize.svg",
+  ];
   return (
     <div>
       <nav>
@@ -22,7 +44,7 @@ export default function IndexPage() {
       </nav>
       <header>
         <div className={style.container} id="about">
-          <div className={style.aboutDescription}>
+          <div>
             <h1>Cody the Pug</h1>
             <p>Hi! I am Cody the Pug. I love kisses and hugs.</p>
 
@@ -101,6 +123,81 @@ export default function IndexPage() {
             />
           </div>
         </div>
+      </div>
+      <p>Tech Stack</p>
+      <div className="tech-stack">
+        <StaticImage
+          src="../images/tech/html5.svg"
+          alt="html icon"
+          placeholder="blurred"
+          layout="fixed"
+          style={{
+            height: "50px",
+          }}
+        />
+        <StaticImage
+          src="../images/tech/css3.svg"
+          alt="html icon"
+          placeholder="blurred"
+          layout="fixed"
+          style={{
+            height: "50px",
+          }}
+        />
+        <StaticImage
+          src="../images/tech/javascript.svg"
+          alt="html icon"
+          placeholder="blurred"
+          layout="fixed"
+          style={{
+            height: "50px",
+          }}
+        />
+        <StaticImage
+          src="../images/tech/react.svg"
+          alt="html icon"
+          placeholder="blurred"
+          layout="fixed"
+          style={{
+            height: "50px",
+          }}
+        />
+        <StaticImage
+          src="../images/tech/redux.svg"
+          alt="html icon"
+          placeholder="blurred"
+          layout="fixed"
+          style={{
+            height: "50px",
+          }}
+        />
+        <StaticImage
+          src="../images/tech/postgresql.svg"
+          alt="html icon"
+          placeholder="blurred"
+          layout="fixed"
+          style={{
+            height: "50px",
+          }}
+        />
+        <StaticImage
+          src="../images/tech/expressjs.svg"
+          alt="html icon"
+          placeholder="blurred"
+          layout="fixed"
+          style={{
+            height: "50px",
+          }}
+        />
+        <StaticImage
+          src="../images/tech/node.svg"
+          alt="html icon"
+          placeholder="blurred"
+          layout="fixed"
+          style={{
+            height: "50px",
+          }}
+        />
       </div>
     </div>
   );
